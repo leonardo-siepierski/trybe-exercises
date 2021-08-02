2bb3a9c3f9dccbde13e4c00ac3c3b6a0e81a7a05
leonardo <leonardo.siepierski@outlook.com>
2021-08-02 14:49:06 -0300

    Parte dos exercicios do dia terminada

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/dia-2.js
@@ -0,0 +1,71 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
+let somaMedia = 0;
+let media2 = 0;
+let somaMedia2 = 0;
+
+//exerc 1 imprime os valores do array
+// for (index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index]);
+// }
+
+//exerc 2 soma os valores do array e imprime a soma
+
+// for (indexSoma = 0; indexSoma < numbers.length; indexSoma += 1) {
+//   soma += numbers[indexSoma];
+// }
+// console.log(soma);
+
+//exerc 3 imprime a media dos valores do array
+
+// for (indexMedia = 0; indexMedia < numbers.length; indexMedia += 1) {
+//   somaMedia += numbers[indexMedia];
+//   media = somaMedia / numbers.length;
+// }
+// console.log(media);
+
+//exerc 4 se media maior que 20 imprime maior que 20 se menor igual menor igual a 20
+
+// for (indexMedia2 = 0; indexMedia2 < numbers.length; indexMedia2 += 1) {
+//   somaMedia2 += numbers[indexMedia2];
+//   media2 = somaMedia2 / numbers.length;
+// }
+// if (media2 > 20) {
+// console.log("Valor maior que 20");
+// }
+// else {
+// console.log("Valor menor ou igual a 20");
+// }
+
+//exerc 5 imprime o maior valor do array
+
+// let higherNumber = numbers[0];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] > higherNumber) {
+//     higherNumber = numbers[index];
+//   }
+// }
+
+// console.log(higherNumber);
+
+//exerc 6 imprime a quantidade de numeros impares e "nenhum valor impar encontrado" caso false
+
+// let impares = 0;
+
+// for (let contagemImpares = 0; contagemImpares < numbers.length; contagemImpares += 1) {
+//  if (numbers[contagemImpares] % 2 !== 0) {
+//   impares += 1;
+//  }
+// }
+
+// if (impares == 0) {
+//   console.log("Nenhum valor impar encontrado");
+// }
+// else {
+//   console.log(impares);
+// }
+
+//exerc 7 imprime o menor valor do array
+
